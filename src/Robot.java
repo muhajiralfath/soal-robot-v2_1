670a9afae12b50c9726f6fa879213deb51f38f51
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-06-13 21:58:26 +0700

    [FEATURE] add method move in class Robot

--- src/Robot.java
+++ src/Robot.java
@@ -8,5 +8,16 @@ public class Robot {
         this.y = y;
         this.direction = direction;
     }
+    public void move(){
+        if(direction.equals("NORTH")){
+            y++;
+        }else if (direction.equals("EAST")){
+            x++;
+        }else if(direction.equals("SOUTH")){
+            y--;
+        }else if (direction.equals("WEST")){
+            x--;
+        }
+    }
 
 }
