bcae74a1acc120c6c8c97cc1f2ae496c730d43ea
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-06-13 21:57:40 +0700

    [FEATURE] make class Robot

---
+++ src/Robot.java
@@ -0,0 +1,12 @@
+public class Robot {
+    private int x;
+    private int y;
+    private String direction;
+
+    Robot(String direction, int x, int y){
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+}
--- src/RobotArena.java
+++ src/RobotArena.java
@@ -4,13 +4,13 @@ public class RobotArena {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Tentukan posisi awal robot : ");
+        System.out.print("Tentukan posisi awal robot : ");
         String[] inputInitialPosition = scan.nextLine().split(",");
         String direction = inputInitialPosition[0].toUpperCase();
         int x = Integer.parseInt(inputInitialPosition[1]);
         int y = Integer.parseInt(inputInitialPosition[2]);
 
-        System.out.println("Input perintah : ");
+        System.out.print("Input perintah : ");
         String commands = scan.nextLine().toUpperCase();
 
 
