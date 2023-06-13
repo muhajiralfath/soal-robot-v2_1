298203d761379c35ec87443dfb6a9a6dca932d26
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-06-13 23:40:50 +0700

    [REFACTORING] add method inputValidation & inputCommands

--- src/RobotArena.java
+++ src/RobotArena.java
@@ -3,18 +3,54 @@ import java.util.Scanner;
 public class RobotArena {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String direction, commands;
+        int x, y;
+        String[] inputInitialPosition;
 
-        System.out.print("Tentukan posisi awal robot : ");
-        String[] inputInitialPosition = scan.nextLine().split(",");
-        String direction = inputInitialPosition[0].toUpperCase();
-        int x = Integer.parseInt(inputInitialPosition[1]);
-        int y = Integer.parseInt(inputInitialPosition[2]);
+        do {
+            System.out.println("Masukkan Tiga input berupa arah, x, y! contoh (EAST,2,3)");
+            System.out.print("Tentukan posisi awal robot : ");
+            inputInitialPosition = scan.nextLine().split(",");
+            direction = inputInitialPosition[0].toUpperCase();
+            if (inputValidation(scan, inputInitialPosition)) break;
+        } while (true);
 
-        System.out.print("Input perintah : ");
-        String commands = scan.nextLine().toUpperCase();
+        commands = inputCommands(scan);
+
+        x = Integer.parseInt(inputInitialPosition[1]);
+        y = Integer.parseInt(inputInitialPosition[2]);
 
         Robot robot = new Robot(direction, x, y);
         robot.doCommands(commands);
+    }
 
+    private static String inputCommands(Scanner scan) {
+        String commands;
+        do {
+            System.out.print("Input perintah : ");
+            commands = scan.nextLine().toUpperCase();
+
+            if (!commands.matches("^[ARLarl]+$")) {
+                System.err.println("Input Salah! MASUKAN A = Jalan. R = Belok Kanan, L = Belok Kiri. Ulangi!");
+                System.err.println("Tekan Enter ....");
+                scan.nextLine();
+            } else {
+                break;
+            }
+        } while (true);
+        return commands;
+    }
+
+    private static boolean inputValidation(Scanner scan, String[] inputInitialPosition) {
+        if ((inputInitialPosition[0].equalsIgnoreCase("EAST") || inputInitialPosition[0].equalsIgnoreCase("WEST") || inputInitialPosition[0].equalsIgnoreCase("NORTH") || inputInitialPosition[0].equalsIgnoreCase("SOUTH"))
+                && !inputInitialPosition[1].matches(".*[a-zA-Z].*")
+                && !inputInitialPosition[2].matches(".*[a-zA-Z].*")) {
+            return true;
+        } else {
+            System.out.println();
+            System.err.println("Input Salah! Ulangi! Tekan Enter .....");
+            scan.nextLine();
+        }
+        return false;
     }
 }
